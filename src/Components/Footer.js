4b3a5ae78850f348ca9f5c35fc634f453da55c9f
jenghua <zheng41457@gmail.com>
2020-09-29 08:56:23 +0800

    Js

---
+++ src/Components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Footer(){
+    return(
+        <header className="bg-gray-200 text-center text-xs p-3 w-full">
+            &copy; Copyright 2020
+        </header>
+    )
+}
+export default Footer
